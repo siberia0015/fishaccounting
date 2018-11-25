4b24aa439f0e8e07c488cc5871c79db92137e01e
EventideX <844058637@qq.com>
2018-11-25 08:03:34 +0800

    feat:add fragments.

--- app/src/main/java/com/fishfishfish/fishaccounting/ui/activity/MainActivity.java
+++ app/src/main/java/com/fishfishfish/fishaccounting/ui/activity/MainActivity.java
@@ -1,15 +1,246 @@
 package com.fishfishfish.fishaccounting.ui.activity;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.fishfishfish.fishaccounting.R;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    protected static final int USERINFOACTIVITY_CODE = 0;
+    protected static final int LOGINACTIVITY_CODE = 1;
+    @BindView(R.id.toolbar)
+    android.support.v7.widget.Toolbar toolbar;
+    @BindView(R.id.tablayout)
+    TabLayout tabLayout;
+    @BindView(R.id.main_viewpager)
+    ViewPager viewPager;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawer;
+    @BindView(R.id.nav_view)
+    NavigationView navigationView;
+    private View drawerHeader;
+    private ImageView drawerIv;
+    private TextView drawerTvAccount, drawerTvMail;
+    // Tab
+    private FragmentManager mFragmentManager;
+    private MainFragmentPagerAdapter mainFragmentPagerAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getLayout() {
+        return R.layout.activity_main;
     }
+
+    @Override
+    protected void initEventAndData() {
+
+        //第一次进入将默认账单分类添加到数据库
+        if (SharedPUtils.isFirstStart(mContext)) {
+            Log.i(TAG, "第一次进入将默认账单分类添加到数据库");
+            NoteBean note = new Gson().fromJson(Constants.BILL_NOTE, NoteBean.class);
+            List<BSort> sorts = note.getOutSortlis();
+            sorts.addAll(note.getInSortlis());
+            LocalRepository.getInstance().saveBsorts(sorts);
+            LocalRepository.getInstance().saveBPays(note.getPayinfo());
+        }
+
+        //初始化ViewPager
+        mFragmentManager = getSupportFragmentManager();
+        mainFragmentPagerAdapter = new MainFragmentPagerAdapter(mFragmentManager);
+        mainFragmentPagerAdapter.addFragment(new MonthDetailFragment(), "账目");
+        mainFragmentPagerAdapter.addFragment(new MonthChartFragment(), "报表");
+        mainFragmentPagerAdapter.addFragment(new MonthAccountFragment(), "钱包");
+
+        viewPager.setAdapter(mainFragmentPagerAdapter);
+
+        //初始化TabLayout
+        tabLayout.addTab(tabLayout.newTab().setText("账目"));
+        tabLayout.addTab(tabLayout.newTab().setText("报表"));
+        tabLayout.addTab(tabLayout.newTab().setText("钱包"));
+        tabLayout.setupWithViewPager(viewPager);
+
+        //初始化Toolbar
+        toolbar.setTitle("CocoBill");
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        drawerHeader = navigationView.inflateHeaderView(R.layout.drawer_header);
+        drawerIv = (ImageView) drawerHeader.findViewById(R.id.drawer_iv);
+        drawerTvAccount = (TextView) drawerHeader.findViewById(R.id.drawer_tv_name);
+        drawerTvMail = (TextView) drawerHeader.findViewById(R.id.drawer_tv_mail);
+        //监听点击登陆事件
+        drawerHeader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                drawer.closeDrawer(GravityCompat.START);
+                if (currentUser == null) {
+                    //用户id为0表示未有用户登陆
+                    startActivityForResult(new Intent(MainActivity.this, LoginActivity.class), LOGINACTIVITY_CODE);
+                } else {
+                    //进入账户中心
+                    startActivityForResult(new Intent(MainActivity.this, UserInfoActivity.class), USERINFOACTIVITY_CODE);
+                }
+            }
+        });
+        //设置头部账户
+        setDrawerHeaderAccount();
+        //监听侧滑菜单
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    /**
+     * 设置DrawerHeader的用户信息
+     */
+    public void setDrawerHeaderAccount() {
+        currentUser = BmobUser.getCurrentUser(MyUser.class);
+        //获取当前用户
+        if (currentUser != null) {
+            drawerTvAccount.setText(currentUser.getUsername());
+            drawerTvMail.setText(currentUser.getEmail());
+            Glide.with(mContext).load(currentUser.getImage()).into(drawerIv);
+        } else {
+            drawerTvAccount.setText("账号");
+            drawerTvMail.setText("点我登陆");
+            drawerIv.setImageResource(R.mipmap.ic_def_icon);
+        }
+    }
+
+    /**
+     * 显示修改主题色 Dialog
+     */
+    private void showUpdateThemeDialog() {
+        final String[] themes = ThemeManager.getInstance().getThemes();
+        new AlertDialog.Builder(mContext)
+                .setTitle("选择主题")
+                .setItems(themes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ThemeManager.getInstance().setTheme(mContext, themes[which]);
+                    }
+                }).create().show();
+    }
+
+    /**
+     * 监听Drawer
+     */
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    /**
+     * 监听点击事件 R.id.drawer_tv_name,R.id.drawer_tv_mail
+     *
+     * @param view
+     */
+    @OnClick({})
+    public void onClick(View view) {
+        switch (view.getId()) {
+
+            default:
+                break;
+        }
+    }
+
+    /**
+     * 监听Activity返回值
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case USERINFOACTIVITY_CODE:
+                    setDrawerHeaderAccount();
+                    break;
+                case LOGINACTIVITY_CODE:
+                    setDrawerHeaderAccount();
+                    break;
+            }
+        }
+    }
+
+    /**
+     * 监听侧滑菜单事件
+     *
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.nav_account) {      //账户
+            // Handle the camera action
+            viewPager.setCurrentItem(0);
+        } else if (id == R.id.nav_month) {
+            viewPager.setCurrentItem(1);
+        } else if (id == R.id.nav_total) {
+            viewPager.setCurrentItem(2);
+        } else if (id == R.id.nav_sync) {   //同步账单
+            if (currentUser == null)
+                SnackbarUtils.show(mContext, "请先登陆");
+            else
+                BmobRepository.getInstance().syncBill(currentUser.getObjectId());
+        } else if (id == R.id.nav_setting) {   //设置
+            startActivity(new Intent(this, SettingActivity.class));
+        } else if (id == R.id.nav_about) {     //关于
+            startActivity(new Intent(MainActivity.this, AboutActivity.class));
+        } else if (id == R.id.nav_theme) {     //主题
+            showUpdateThemeDialog();
+        } else if (id == R.id.nav_exit) {      //退出登陆
+            //退出登陆
+            new AlertDialog.Builder(mContext).setTitle("是否退出当前账户")
+                    .setNegativeButton("取消", null)
+                    .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            MyUser.logOut();
+                            //清除本地数据
+                            LocalRepository.getInstance().deleteAllBills();
+                            //刷新账户数据
+                            initEventAndData();
+                        }
+                    })
+                    .show();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }
